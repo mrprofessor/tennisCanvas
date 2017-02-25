dc9ec419f4c543073121868f216e9d1e43868d6e
Rudra Narayan <kar.rudra008@gmail.com>
2017-02-26 01:38:44 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,15 +1,37 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
+var ballSpeedX = 7;
 
 window.onload = function (){
         canvas = document.getElementById('gameCanvas');
         canvasContext = canvas.getContext('2d');
-        drawEverything();
+        var framePerSecond = 30;
+        setInterval(function(){
+            moveEverything();
+            drawEverything();
+        }, 1000/framePerSecond);
 }
-
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    if(ballX > canvas.width+10 || ballX < 10){
+        ballSpeedX = -ballSpeedX;
+    }
+}
 function drawEverything() {
-    canvasContext.fillStyle = 'teal';
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(100,200,50,50);
+    colorRect(0,0,canvas.width,canvas.height,'teal');
+    colorRect(0,210,10,100,'pink');
+    colorRect(canvas.width-10,210,10,100,'pink');
+
+    colorCircle(ballX,100,10, 'red');
+}
+function colorRect(leftX,rightY,width,height,drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX,rightY,width,height);
+}
+function colorCircle(centerX, centerY, radius, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
 }
