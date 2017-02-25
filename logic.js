8d1cbef57fb7c7a2c7a0098c761cec348f9a35d4
Rudra Narayan <kar.rudra008@gmail.com>
2017-02-25 19:43:34 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,10 +1,15 @@
 var canvas;
 var canvasContext;
 
-window.onload = function() {
-    console.log("Hello JS");
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+window.onload = function (){
+        canvas = document.getElementById('gameCanvas');
+        canvasContext = canvas.getContext('2d');
+        drawEverything();
+}
+
+function drawEverything() {
     canvasContext.fillStyle = 'teal';
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(100,200,50,50);
 }
