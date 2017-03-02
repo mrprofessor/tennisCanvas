75fb3ba7372b5ec8ab3a46d1df47c211fd19d25e
Mrprofessor <kar.rudra008@gmail.com>
2017-03-03 00:21:12 +0530

    Commit #0026 EOD

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
-var bally = 50;
+var ballY = 50;
 var ballSpeedX = 10;
 var ballSpeedY = 4;
 
@@ -43,41 +43,47 @@ window.onload = function (){
 function ballReset() {
     ballSpeedX = -ballSpeedX;
     ballX = canvas.width/2;
-    bally = canvas.height/2;
+    ballY = canvas.height/2;
 }
 function computerMovement() {
     var paddle2YCenter = paddle2Y + PADDLE_HEIGHT/2;
-    if(paddle2YCenter < bally-35) {
-        paddle2Y += 10;
-    } else if(paddle2YCenter > bally+35){
-        paddle2Y -= 10;
+    if(paddle2YCenter < ballY-35) {
+        paddle2Y += 5;
+    } else if(paddle2YCenter > ballY+35){
+        paddle2Y -= 5;
     }
 
 }
 function moveEverything() {
     computerMovement();
     ballX += ballSpeedX;
-    bally += ballSpeedY;
+    ballY += ballSpeedY;
     if(ballX > canvas.width){
-        if(bally > paddle2Y && bally < paddle2Y+PADDLE_HEIGHT){
+        if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT){
             ballSpeedX = -ballSpeedX;
+
+            var deltaY = ballY - (paddle2Y+PADDLE_HEIGHT/2)
+            ballSpeedY = deltaY * 0.35;
         } else {
             ballReset();
-            player1Score--;
+            player1Score++;
         }
     }
     if (ballX < 0) {
-        if(bally > paddle1Y && bally < paddle1Y+PADDLE_HEIGHT){
+        if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT){
             ballSpeedX = -ballSpeedX;
+
+            var deltaY = ballY - (paddle1Y+PADDLE_HEIGHT/2)
+            ballSpeedY = deltaY * 0.35;
         } else {
             ballReset();
             player2Score++;
         }
     }
-    if(bally > canvas.height) {
+    if(ballY > canvas.height) {
         ballSpeedY = -ballSpeedY;
     }
-    if(bally < 0) {
+    if(ballY < 0) {
         ballSpeedY = -ballSpeedY;
     }
 }
@@ -85,15 +91,18 @@ function drawEverything() {
     colorRect(0,0,canvas.width,canvas.height,'teal');
 
     //Left player paddle
-    colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'pink');
+    colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'yellow');
 
     //Right Computer Paddle
-    colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'pink');
+    colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'yelow');
 
-    colorCircle(ballX, bally, 10, 'red');
+    colorCircle(ballX, ballY, 10, 'white');
 
     //score
-    canvasContext.font = '20px serif'
+    canvasContext.font = '20px monokai'
+    canvasContext.fillText("player1", 80,30);
+    canvasContext.fillText("player2", canvas.width-130, 30);
+    canvasContext.font = '30px serif'
     canvasContext.fillText(player1Score, 100,100);
     canvasContext.fillText(player2Score, canvas.width-100, 100);
 }
